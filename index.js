9766cdc71da95cc60f02337bce6d3d1241550ecb
wallace <wsilvasjb@gmail.com>
2017-05-10 20:25:17 -0300

    adicionado suporte a envio de sms

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ module.exports = {
      * @return Promisse
      */
     sendOne : require("./lib/send_sms"),
+
     /**
      * @return Promisse
      */
     getStatus: require("./lib/get_sms_status"),
+
     /**
      * @return Promisse
      */
