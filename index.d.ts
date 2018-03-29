b0bab1174b72caecd119456e9c51b88eec9829a6
wallacesilva <wsilva@gmail.com>
2018-03-29 14:15:49 -0300

    implementado suporte ao typescript

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+declare module "centeralarm-auth" {
+    export function sendOne(conta: string, senha: string, body: object): Promise<object>;
+    export function getStatus(conta: string, senha: string, id: any): Promise<object>;
+    export function list(conta: string, senha: string): Promise<IReceivedResponse>;
+}
+
+declare interface ISendResponse {
+    "sendSmsResponse": {
+        "statusCode": string;
+        "statusDescription": string;
+        "detailCode": string
+        "detailDescription": string;
+    }
+}
+
+declare interface IReceivedMessages {
+    "id": number
+    "dateReceived": any;
+    "mobile": string;
+    "body": string;
+    "shortcode": string;
+    "mobileOperatorName": string;
+    "mtId": string;
+}
+
+declare interface IReceivedResponse {
+    "receivedResponse": {
+        "statusCode": string;
+        "statusDescription": string;
+        "detailCode": string;
+        "detailDescription": string;
+        "receivedMessages": IReceivedMessages[] | null;
+    }
+}
